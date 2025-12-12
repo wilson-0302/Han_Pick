e09edd0cf22834cfd181087cf7468246c15f0c9c
wilson-0302 <gksmf030302@handong.ac.kr>
2025-12-12 10:41:15 +0900

    db VO

---
+++ src/main/java/com/exmaple/han_pick/vo/BoardVO.java
@@ -0,0 +1,17 @@
+package com.exmaple.han_pick.vo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class BoardVO {
+    private int id;
+    private String title;
+    private String writer;
+    private String category;
+    private String content;
+    private String img;
+    private int recruitCount;
+    private LocalDate deadline;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/exmaple/han_pick/vo/MemberVO.java
@@ -0,0 +1,14 @@
+package com.exmaple.han_pick.vo;
+
+import java.time.LocalDateTime;
+
+public class MemberVO {
+    private Long memberId;
+    private String loginId;
+    private String password;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
